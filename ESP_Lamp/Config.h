fc717eb93c4f34582f9434a5f2022950050f908d
daiki <51706588+KARLL25@users.noreply.github.com>
2023-11-01 19:22:32 +0800

    Update Config.h

--- ESP_Lamp/Config.h
+++ ESP_Lamp/Config.h
@@ -1,13 +1,11 @@
-// THIS is Config.h
-String ssid_AP = "ESP_WIFI_daiki"; // AP for access point/router
-String ssid_pass = "ESP_8266_123"; // 8 chars at least
+String ssid_AP = "ESPDaiki"; 
+String ssid_pass = "ESP8266123"; 
 
-
+//wifi client
 const char* ssid_client = "1Redmi";
 const char* pass_client = "1223334444";
 
 int web_port = 80;
 
-int led_pin = LED_BUILTIN;
 char* mqtt_broker = "broker.emqx.io";
 int mqtt_port = 1883;
