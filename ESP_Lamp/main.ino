f6461f1362140d8fe03171d266ac5390455876d3
daiki <51706588+KARLL25@users.noreply.github.com>
2023-11-01 19:22:11 +0800

    Update main.ino

--- ESP_Lamp/main.ino
+++ ESP_Lamp/main.ino
@@ -3,19 +3,28 @@
 #include "SERVER.h"
 #include "MQTT.h"
 
+
 void setup() {
   Serial.begin(9600);
-  pinMode(led_pin, OUTPUT);
-  // start_AP_mode();
-  init_WIFI(false);
-  server_init();
-  init_MQTT();
-  String state_topic = "esp8286-" + id() + "/state";
-  String command_topic = "esp8286-" + id() + "/command";
-  mqtt_client.publish(state_topic.c_str(), "Hello");
-  mqtt_client.subscribe(command_topic.c_str());
-  
+  Serial.println();
+  pinMode(LED_BUILTIN, OUTPUT);
+  start_AP_mode();
+  bool wifi_is_on = init_WIFI(false);
+  if (wifi_is_on) {
+    Serial.println(" working");
+    init_MQTT();
+    server_init();
+  } else {
+    Serial.println("not working");
+  }                         
+  String topic = "esp8266daiki/command";
+  String state_topic = "esp8266daiki/state";
+  mqtt_client.subscribe(topic.c_str());
+  mqtt_client.publish(state_topic.c_str(), "hello");
+  Serial.println("See me at " + state_topic);
 }
+
+
 
 void loop() {
   server.handleClient();
