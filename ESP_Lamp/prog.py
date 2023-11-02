b69df002b825fa1ac56f1d6d7e2bb199fa790bd9
Svyatoslav Lipatov <92099105+svyatoslavlipatov@users.noreply.github.com>
2023-11-02 11:43:39 +0800

    Update prog.py

--- ESP_Lamp/prog.py
+++ ESP_Lamp/prog.py
@@ -1,23 +1,18 @@
 import time
 import paho.mqtt.client as paho
 import datetime
-
 broker = "broker.emqx.io"
-client = paho.Client("client-isu-741")
-
+client = paho.Client("client-isu-111")
 print("Connecting to broker", broker)
 client.connect(broker)
 client.loop_start()
 print("Publishing")
-
-min_duration = 20  # Минимальная длительность свечения
-max_duration = 40  # Максимальная длительность свечения
-current_duration = max_duration  # Начинаем с максимальной длительности
+min_duration = 20 
+max_duration = 40 
+current_duration = max_duration  
 try:
     while True:
-
         now = datetime.datetime.now()
-        # Уменьшаем длительность свечения каждую следующую минуту
         if now.second == 0:
             max_duration -= 1
 
@@ -30,13 +25,11 @@ try:
         else:
             state = "1"
 
-        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-        print(f'Time: {current_time}, state: {state}, светит с {min_duration} по: {max_duration} секунд')
+        current_time = now.strftime("%S")
+        print(f'Секунды: {current_time}')
         client.publish("esp8266daiki/command", state)
-
         time.sleep(1)
-
-
+        
 except KeyboardInterrupt:
     print("\nExiting...")
     client.disconnect()
